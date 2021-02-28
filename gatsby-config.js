b7b4068732155988f80da8cb72f165b3200f3010
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-02-28 10:40:35 -0800

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,29 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+module.exports = {
+    siteMetadata: {
+        title: `Benjamin Lu`,
+        tagline: `WordPress Developer`,
+        primaryMenu: [
+            {
+                name: `Home`,
+                url: `/`,
+            },
+            {
+                name: `About`,
+                url: `/about`,
+            },
+            {
+                name: `Blogs`,
+                url: `/blogs`
+            },
+            {
+                name: `Contact`,
+                url: `/contact`
+            },
+        ],
+    },
+}
